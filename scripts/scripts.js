c6fcb95723186278f2cf57463d18a44024473c36
Luis Gaytan <gaytan_luis@msn.com>
2017-04-20 11:53:07 -0700

    header, primary, and secondary draft complete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,13 +4,19 @@ $(document).ready(function(){
     $('.hero-text').css({
             'transform': 'translateX(' + wScroll / 3 + '%)'
         });
-
     if (wScroll > 150){
       $('footer').removeClass('hidden');
       $('.hero-text').addClass('shade');
     } else {
       $('footer').addClass('hidden');
       $('.hero-text').removeClass('shade');
+    }
+    if (wScroll > 200) {
+      $('.text-center > img').each(function(i){
+        setTimeout(function(){
+          $('.text-center > img').eq(i).css('opacity', '1');
+        }, 250*(i+1));
+      });
     }
   });
 });
